15f227b40ebd835d22b3cda4e1f6ad6a8d53df98
hitenpatel1789 <er.hiten@gmail.com>
2025-12-29 16:44:32 -0500

    application.yml updated

--- src/test/java/com/jpmc/midascore/TaskTwoTests.java
+++ src/test/java/com/jpmc/midascore/TaskTwoTests.java
@@ -26,16 +26,11 @@ class TaskTwoTests {
         for (String transactionLine : transactionLines) {
             kafkaProducer.send(transactionLine);
         }
-        Thread.sleep(2000);
+        Thread.sleep(5000);
         logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
-        logger.info("use your debugger to watch for incoming transactions");
-        logger.info("kill this test once you find the answer");
-        while (true) {
-            Thread.sleep(20000);
-            logger.info("...");
-        }
+        logger.info("Test completed");
     }
 
 }
