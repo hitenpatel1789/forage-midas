c3438f7bd0a8eaee9147b482045881a3f618e94a
hitenpatel1789 <er.hiten@gmail.com>
2025-12-29 17:13:08 -0500

    TaskFourTests updated

--- src/main/java/com/jpmc/midascore/component/KafkaListenerComponent.java
+++ src/main/java/com/jpmc/midascore/component/KafkaListenerComponent.java
@@ -2,19 +2,23 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class KafkaListenerComponent {
     private static final Logger logger = LoggerFactory.getLogger(KafkaListenerComponent.class);
     private final DatabaseConduit databaseConduit;
+    private final RestTemplate restTemplate;
 
-    public KafkaListenerComponent(DatabaseConduit databaseConduit) {
+    public KafkaListenerComponent(DatabaseConduit databaseConduit, RestTemplate restTemplate) {
         this.databaseConduit = databaseConduit;
+        this.restTemplate = restTemplate;
     }
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "${spring.kafka.consumer.group-id}")
@@ -25,13 +29,16 @@ public class KafkaListenerComponent {
         UserRecord recipient = databaseConduit.findUserById(transaction.getRecipientId());
 
         if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
+            Incentive incentive = restTemplate.postForObject("http://localhost:8080/incentive", transaction, Incentive.class);
+            float incentiveAmount = (incentive != null) ? incentive.getAmount() : 0;
+
             sender.setBalance(sender.getBalance() - transaction.getAmount());
-            recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+            recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
 
             databaseConduit.save(sender);
             databaseConduit.save(recipient);
 
-            TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+            TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount);
             databaseConduit.saveTransaction(record);
         } else {
             logger.info("Discarding invalid transaction: {}", transaction);
